a270c54ed86b91aaf12d91544c1d34edb235cdef
NivedSudhakar <nived.s.2018@gmail.com>
2024-07-29 09:20:35 -0700

    added google drive api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,55 @@ import './App.scss';
 import Intro from './components/Intro';
 import { Container } from 'react-bootstrap';
 import ImageGallery from './components/ImageGallery';
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const imageLinks = [];
+  const effectRan = useRef(false);
+
+
+  async function getData() {
+    try {
+        let res = await axios({
+            url: process.env.REACT_APP_GOOGLE_DRIVE_URL,
+            method: 'get',
+            timeout: 8000,
+
+        })
+
+        // Don't forget to return something
+        return res.data
+    }
+    catch (err) {
+        console.error(err);
+    }
+  }
+  useEffect(() => {
+    const abortController = new AbortController();
+    getData()
+      .then(response => {
+          response.items.forEach(element => {
+              imageLinks.push(element.alternateLink.split("/")[5]);
+          });
+
+          setImages(imageLinks);
+          console.log("it works");
+
+      });
+
+    },[])
+
+
+
+
+
   return (
     <div className="App">
       <Container>
         <Intro/>
-        <ImageGallery/>
+        <ImageGallery imageLinks={images}/>
       </Container>
     </div>
   );
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const ImageGallery = () => {
-    const images = require.context('../images', true);
-    const imageList = images.keys().map(image => images(image));
+const ImageGallery = ( {imageLinks} ) => {
+
   return (
     <ul className='image-gallery'>
         {
-            imageList.map((imgUrl, index) => {
+            imageLinks.map((imgUrl, index) => {
                 return (
                     <li className={index % 2 === 0 ? 'item short' : 'item tall'}>
                         <figure>
-                            <img src="https://drive.google.com/thumbnail?id=1_7m9A7EDiVw5Rd4ozN94tzfuObtTi0uX&sz=w1000" alt=""/>
+                            <img src={`https://drive.google.com/thumbnail?id=${imgUrl}&sz=w1000`} alt=""/>
 
                         </figure>
                     </li>
@@ -20,8 +21,11 @@ const ImageGallery = () => {
             /*<img src={imgUrl} alt='a fantastic picture of Monk' />*/
         }
 
+
     </ul>
   )
 }
 
 export default ImageGallery
+//1_7m9A7EDiVw5Rd4ozN94tzfuObtTi0uX'
+//1_7m9A7EDiVw5Rd4ozN94tzfuObtTi0uX
